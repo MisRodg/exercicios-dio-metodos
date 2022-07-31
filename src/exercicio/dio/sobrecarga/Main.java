d239fadf01f9fcfc92b12373d86139fa12ac1b04
MisRodg <mislene.mrr@gmail.com>
2022-07-30 23:37:39 -0300

    criando package exercício.dio.sobrecarga

---
+++ src/exercicio/dio/sobrecarga/Main.java
@@ -0,0 +1,14 @@
+package exercicio.dio.sobrecarga;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Quadrilatero
+        System.out.println("Exercício quadrilátero");
+        Quadrilateros.area(9);
+        Quadrilateros.area(9, 5);
+        Quadrilateros.area(15, 10, 8);
+
+    }
+}
